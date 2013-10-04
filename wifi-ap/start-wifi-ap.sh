d8f006e8ec59dc7ff35e29279dc296143c0eade7
Szabolcs Berecz <berecz.szabolcs@ustream.tv>
2013-10-04 14:46:27 +0200

    Make wifi-ap more robust

--- wifi-ap/start-wifi-ap.sh
+++ wifi-ap/start-wifi-ap.sh
@@ -1,12 +1,22 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # The following packages are required: hostapd, bridge-utils
+
+set -e
+
+[ `id -u` -eq 0 ] || {
+	echo "You need to run this script as root!"
+	exit 1
+}
+
+SCRIPT_DIR=`dirname $0`
+HOSTAPD_CONF=$SCRIPT_DIR/hostapd.conf
 
 brctl addbr br0
 ifconfig eth0 0.0.0.0
 brctl addif br0 eth0
 
-hostapd -B hostapd.conf
+hostapd -B $HOSTAPD_CONF
 
 echo "The WPA password is:"
-grep ^wpa_passphrase hostapd.conf
+grep ^wpa_passphrase $HOSTAPD_CONF
--- wifi-ap/stop-wifi-ap.sh
+++ wifi-ap/stop-wifi-ap.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+set -e
+
+[ `id -u` -eq 0 ] || {
+	echo "You need to run this script as root!"
+	exit 1
+}
 
 killall hostapd
 
@@ -6,4 +13,3 @@ brctl delif br0 eth0
 ifconfig br0 down
 brctl delbr br0
 
-echo "Your network is now dead :)"
